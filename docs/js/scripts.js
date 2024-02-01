d27f6ca40e97cb61400d0e00cde5accb6454f402
ManiacOfGitHub <43019219+ManiacOfGitHub@users.noreply.github.com>
2024-01-31 19:34:36 -0500

    initial commit

---
+++ docs/js/scripts.js
@@ -0,0 +1,56 @@
+/* http://stackoverflow.com/a/5077091 */
+String.prototype.format = function () {
+  var args = arguments;
+  return this.replace(/\{(\d+)\}/g, function (m, n) { return args[n]; });
+};
+var is_ej;
+
+$('button.group').on('click', function() {
+  if ($(this).hasClass('selected')) {
+    $(this).removeClass('selected');
+  } else {
+    $(this).siblings().removeClass('selected');
+    $(this).addClass('selected');
+  }
+
+  // Hide/show relevant system versions
+  $('.firmware .group').each(function (index, elem) {
+    var hide_kor = $(elem).hasClass('no_kor') && $('#kor').hasClass('selected');
+    is_ej = $('.ej').hasClass('selected');
+    var hide_ej = $(elem).hasClass('no_ej') && is_ej;
+    if (hide_kor || hide_ej) {
+      $(elem).removeClass('selected');
+      $(elem).hide();
+    } else {
+      $(elem).show();
+    }
+  })
+  if($(".only_ej") && is_ej) {
+    $(".only_ej").show();
+  } else {
+    $(".only_ej").removeClass('selected');
+    $(".only_ej").hide();
+  }
+
+  if (   $('.region').children().hasClass('selected')
+      && $('.firmware').children().hasClass('selected')
+  ) {
+    $('#download').addClass('active');
+  } else {
+    $('#download').removeClass('active');
+  }
+
+});
+
+$('#download').on('click', function() {
+  if (!$(this).hasClass('active')) {
+    return;
+  }
+
+  var region = $('.region').children('.selected').attr('id');
+  var firmware_ = $('.firmware').children('.selected').attr('id');
+
+  var base = "../"; //This should obviously be updated when/if it goes live.
+  var filename = "super-skaterhax-{0}-{1}.zip".format(region, firmware_);
+  window.location.href = base + filename;
+});
